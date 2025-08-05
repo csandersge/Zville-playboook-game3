09314d33dfe885fe4406584aa5b7ef1f70b18cc8
csandersge <csander.sge@gmail.com>
2025-08-05 17:44:45 +0000

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Zville-playbook-game3/',
   plugins: [react()],
   test: {
     globals: true,
